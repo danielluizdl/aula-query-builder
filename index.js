990c4038ccc2681e64c3e155a377cb5704f91109
Daniel Luiz <loureirodlg@gmail.com>
2024-05-14 19:03:38 -0300

    exemplos de algumas funcoes

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.get('/', async (req, res) => {
     // const agenda = await knex('agenda').whereNull('email')
     // const agenda = await knex('agenda').whereNotNull('email')
     // const agenda = await knex('agenda').whereBetween('id', [10, 13]).orderBy('id', 'desc')
-    const agenda = await knex('agenda').distinct('email')
+    // const agenda = await knex('agenda').distinct('email')
+
+    // const agenda = await knex('agenda').whereNull('email').count('id')
+    // const agenda = await knex('agenda').whereNull('email').sum('id')
+    // const agenda = await knex('agenda').whereNull('email').avg('id')
+    // const agenda = await knex('agenda').whereNull('email').min('id')
+    const agenda = await knex('agenda').whereNull('email').max('id')
+
     return res.json(agenda);
 })
 
