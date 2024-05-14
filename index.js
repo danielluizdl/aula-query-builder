2973e1d85a1275699f1a64f2f2f73274ac261a9d
Daniel Luiz <loureirodlg@gmail.com>
2024-05-14 18:58:03 -0300

    aprofundando where

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.get('/', async (req, res) => {
     // const agenda = await knex.raw('agenda')('select * from agenda')
     // const agenda = await knex('agenda').where('id', 5)
     // const agenda = await knex('agenda').where('id', !=, 5)
-    const agenda = await knex('agenda').where({ id: 5 })
+    // const agenda = await knex('agenda').where({ id: 5 }).first()
+    // const agenda = await knex('agenda').whereNull('email')
+    // const agenda = await knex('agenda').whereNotNull('email')
+    // const agenda = await knex('agenda').whereBetween('id', [10, 13]).orderBy('id', 'desc')
+    const agenda = await knex('agenda').distinct('email')
     return res.json(agenda);
 })
 
