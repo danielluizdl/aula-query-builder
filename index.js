e008ea8103fd5ce9a503743a8ee49aa88605fc60
Daniel Luiz <loureirodlg@gmail.com>
2024-05-14 17:54:31 -0300

    first commit w/knex

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const knex = require('./conexao');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const agenda = await knex('agenda')
+    return res.json(agenda);
+})
+
+app.listen(3000);
